cd5876c7d2a697e269399f24e33ff8e7d9e90e3a
Giulianno Sbrugnera <giulianno@opus-software.com.br>
2016-09-30 11:28:18 -0300

    Define the web page that hosts the application

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 @Component({
     selector: 'my-app',
     template: '<h1>My First Angular App</h1>'
 })
-export class AppComponent { }
+export class AppComponent {
+}
