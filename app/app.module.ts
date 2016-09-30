9e7f8828f73e21cf01be62919df31abb66fd07bf
Giulianno Sbrugnera <giulianno@opus-software.com.br>
2016-09-30 11:15:46 -0300

    Create a component and add it to the application

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,9 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppComponent }   from './app.component';
 @NgModule({
-  imports:      [ BrowserModule ]
+  imports:      [ BrowserModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
